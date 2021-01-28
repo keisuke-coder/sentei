1b37ae4ce7d3879ebc72fbb13f1ca936deb14fd2
keisuke <keisukent0228@i.softbank.jp>
2021-01-28 16:31:11 +0900

    Profileコントローラのnewとcreateを設定

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,8 +3,24 @@ class ProfilesController < ApplicationController
 
   end
 
-  def show
+  def new
+    @profile = Profile.new
+  end
 
+  def create  
+    @profile = Profile.create(profile_params)
+    @profile.user_id = current_user.id
+    if @profile.save
+      redirect_to root_path
+    end
+  end
+
+  def show
+    
   end
   
+  private
+  def profile_params
+    params.require(:profile).permit(:company, :name, :employee_num, :start_time, :ending_time, :regular_holiday, :tel, :first_postal_code, :last_postal_code, :address, :explanation)
+  end
 end
